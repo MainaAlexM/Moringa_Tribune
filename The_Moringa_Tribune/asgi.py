1769454d761b11088a05d79f11605a2004c2c4fc
Mathenge-Alex <arxmaina@gmail.com>
2022-05-24 13:45:02 +0300

    Init: Initial Commit

---
+++ The_Moringa_Tribune/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for The_Moringa_Tribune project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'The_Moringa_Tribune.settings')
+
+application = get_asgi_application()
